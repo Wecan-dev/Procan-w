d7102e446dba55bce933f7f6167f46ec7cbbad18
juan-eduar <chirinoj180918@gmail.com>
2020-02-10 16:18:36 -0400

    Wp theme

--- wordpress/wp-content/themes/procan/index.php
+++ wordpress/wp-content/themes/procan/index.php
@@ -5,6 +5,7 @@
 <?php get_template_part('partials/about'); ?>
 <?php get_template_part('partials/recomendacion'); ?>
 <?php get_template_part('partials/team'); ?>
+<?php get_template_part('partials/products'); ?>
 <?php get_template_part('partials/contact'); ?>
 
 <?php get_footer(); ?>
---
+++ wordpress/wp-content/themes/procan/partials/products.php
@@ -0,0 +1,92 @@
+<section id="team" class="main-team container">
+	<div class="main-team__title">
+		<p class="team-title">
+			Equipo
+		</p>
+	</div>
+	<div class="main-team__text">
+		<p class="text-team">
+			Para adiestrar a un buen can, es necesario estar con los mejores, capacitados en el entrenamiento de perros para tu seguridad.
+		</p>
+	</div>
+	<div class="main-team__slick"> 
+		<div class="main-team__content">
+			<div class="main-team__items">
+				<div class="main-team__card">
+					<div class="main-team__img">
+						<img class="img-team" src="<?php echo get_template_directory_uri(); ?>/assets/img/Gerente.jpeg">
+					</div>
+					<div class="main-team-card__body">
+						<div class="main-team-card__title">
+							<p class="team-card__title">
+								Claudio Godoy Valenzuela
+							</p>
+						</div>
+						<div class="main-team-card__text">
+							<p class="team-card__text">
+								Gerente de Operaciones
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="main-team__items">
+				<div class="main-team__card">
+					<div class="main-team__img">
+						<img class="img-team" src="<?php echo get_template_directory_uri(); ?>/assets/img/adiestramiento.jpeg">
+					</div>
+					<div class="main-team-card__body">
+						<div class="main-team-card__title">
+							<p class="team-card__title">
+								Alex Ceballos Samora
+							</p>
+						</div>
+						<div class="main-team-card__text">
+							<p class="team-card__text">
+								Director de Adiestramiento
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="main-team__items">
+				<div class="main-team__card">
+					<div class="main-team__img">
+						<img class="img-team" src="<?php echo get_template_directory_uri(); ?>/assets/img/Crianza.jpeg">
+					</div>
+					<div class="main-team-card__body">
+						<div class="main-team-card__title">
+							<p class="team-card__title">
+								Ivan Velázquez Velázquez
+							</p>
+						</div>
+						<div class="main-team-card__text">
+							<p class="team-card__text">
+								Director de Crianza
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="main-team__items">
+				<div class="main-team__card">
+					<div class="main-team__img">
+						<img class="img-team" src="<?php echo get_template_directory_uri(); ?>/assets/img/Secretaria.jpeg">
+					</div>
+					<div class="main-team-card__body">
+						<div class="main-team-card__title">
+							<p class="team-card__title">
+								Juana Arévalo Orias
+							</p>
+						</div>
+						<div class="main-team-card__text">
+							<p class="team-card__text">
+								Secretaria/Asistente
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
