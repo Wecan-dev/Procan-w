095ff2813e37c3f0725f6e3c436d831a3e0cee9b
juan-eduar <chirinoj180918@gmail.com>
2020-02-11 14:55:27 -0400

    Wp theme

---
+++ wordpress/wp-content/themes/procan/partials/products.php
@@ -0,0 +1,178 @@
+<section class="main-products container ">
+	<div class="main-products__title">
+		<p class="title-products">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+		</p>
+	</div>
+	<div class="main-products__content">
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/arnes.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Arnes
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/arnes2.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Arnes
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/arnes3.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Arnes
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/collares.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Collares
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/collares2.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Collares
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/collares3.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Collares
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/correas.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Correas
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/correas2.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Correas
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/correas3.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Correas
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/bozales3.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Bozales
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/bazales2.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Bozales
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-products__items">
+			<div class="main-products__card">
+				<div class="main-products__img">
+					<img class="img-products" src="assets/img/bozales3.jpg">
+				</div>
+				<div class="main-products__body">
+					<div class="main-title__products">
+						<p class="title-products">
+							Bozales
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
